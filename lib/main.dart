315079bb0b7c61952779b216447662d59602f613
SzaboRebeka <beccahb11@gmail.com>
2025-10-20 14:53:34 +0200

    Initial commit for the Stopwatch App

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() { runApp(const CounterApp()); }
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0;
+
+  void _increment() { //valtozo novelese
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _pause() { //valtozo novelesenek megallitasa
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _reset() { //visszaallitas 0-ra
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Task 1 - Stopwatch')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //kozepre igazit
+            children: <Widget>[
+              Text(
+                '$_counter', //a valtozom meghivasa
+                style: const TextStyle(
+                    fontSize: 60, fontWeight: FontWeight.bold),
+              ),
+
+              //gombok
+              ElevatedButton(
+                onPressed: _increment, //
+                child: const Text('START'),
+              ),
+
+              ElevatedButton(
+                onPressed: _pause,
+                child: const Text('PAUSE'),
+              ),
+
+              ElevatedButton(
+                onPressed: _reset,
+                child: const Text('RESET'),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
